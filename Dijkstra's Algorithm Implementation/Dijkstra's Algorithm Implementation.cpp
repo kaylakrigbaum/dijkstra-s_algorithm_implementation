82c50c82885ca4ddddc1b2681bcd2a693e6f5896
Kayla Krigbaum <krigbaum.kayla8@gmail.com>
2019-12-06 12:36:35 -0600

    setting up initial main function

---
+++ Dijkstra's Algorithm Implementation/Dijkstra's Algorithm Implementation.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <iostream>
+#include <vector> 
+#include <cmath>
+#include <list>
+
+using namespace std;
+
+int main() {
+	string cities[7] = { "STL", "NY", "MA", "CR", "KC", "LA", "MN" };
+	list<pair<string, int>> STL = { ("NY", 12), ("MN", 9), ("KC", 2), ("CR", 10) };
+	list<pair<string, int>> STL = { ("NY", 12), ("MN", 9), ("KC", 2), ("CR", 10) };
+	list<pair<string, int>> STL = { ("NY", 12), ("MN", 9), ("KC", 2), ("CR", 10) };
+	list<pair<string, int>> STL = { ("NY", 12), ("MN", 9), ("KC", 2), ("CR", 10) };
+	list<pair<string, int>> STL = { ("NY", 12), ("MN", 9), ("KC", 2), ("CR", 10) };
+
+	list<pair<string, int>> city_distances[] = { STL, NY, MA, CR, KC, LA, MN }
+		/*{ ("STL", 12), ("MN" ,3), ("MA", 5)},
+		{("NY", 5), ("LA", 7), ("CR", 6)},
+		{("STL", 10), ("MA", 6), ("KC", 11)},
+		{("LA", 13), ("CR", 11), ("STL", 2)},
+		{("MN", 4), ("MA", 7), ("KC", 13)},
+		{("LA", 4), ("STL", 9), ("NY", 3)} };
+		*/
+
+	return 0;
+
+}
